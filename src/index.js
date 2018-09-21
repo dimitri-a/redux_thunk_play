b3d9e5d8649a8e6d244d71d5c165c32a88fb8c54
Dimitri C <couwenberg@gmail.com>
2018-09-22 09:04:50 +1000

    turn off dev tools

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 const store = createStore(
-    klik,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    klik
 )
 
 ReactDOM.render(

3b299e95f86e4fd6d024aca8fff1117b1a062d5a
Dimitri C <couwenberg@gmail.com>
2018-10-03 15:59:59 +1000

    listing repos

--- src/index.js
+++ src/index.js
@@ -1,69 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import { createStore, bindActionCreators, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { connect } from 'react-redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { createStore, bindActionCreators, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { connect } from "react-redux";
+import thunk from "redux-thunk";
+
+class Dumb extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Dumb props passed on from container:", props);
+  }
+
+  handleClick = () => {
+    //console.log('Dumb props passed on from container:', this.props)
+    this.props.getRepos();
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        hi there from Dumb
+        <button onClick={this.handleClick}>hier</button>
+
+        <ul>
+          {this.props.users.data.map(user => <li>{user.name}</li>)}
+        </ul>
 
 
-const action = type => store.dispatch({ type })
-
-
-const Counter = ({ onIncrement,onDecrement, val }) => {
-  return <div>
-    {val}
-    <button onClick={onIncrement}>increase</button>
-    <button onClick={onDecrement}>decrease</button>
-  </div>
-}
-
-
-export const increment = () => ({
-  type: 'INCREMENT'
-})
-
-export const decrement = () => ({
-  type: 'DECREMENT'
-})
-
-const reducer = (state = 0, action) => {
-  debugger
-  switch (action.type) {
-
-    case 'INCREMENT':
-      console.log('reducer: increment')
-      return state + 1
-
-      case 'DECREMENT':
-      console.log('reducer: increment')
-      return state -1
-
-    default:
-      return state
+      </div>
+    );
   }
 }
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const bla = dispatch =>
+  bindActionCreators(
+    {
+      geklikt,
+      getRepos
+    },
+    dispatch
+  );
 
-const render = () => ReactDOM.render(
-  <Counter val={store.getState()}
-    onIncrement={()=>store.dispatch(increment())}
-    onDecrement={()=>store.dispatch(decrement())}
-  />
-  ,
-  document.getElementById('root')
+const mydata = (state) => ({
+  users: state
+})
+
+const Container = connect(
+  mydata,
+  bla
+)(Dumb);
+
+// //reducer
+// const klik = (state={}, action) => {
+//   if (action.type == "GEKLIKT") {
+//     console.log("reducer says GEKLIKT");
+//     return null;
+//   }
+// };
+
+//data reducer
+const data = (state = [], action) => {
+  //console.log('reducer,data=', state);
+  debugger
+  if (action.type === 'RECEIVED_DATA') {
+    return action.payload;
+  }
+  else
+    return state;
+};
+
+
+
+
+//actioncreator
+const geklikt = () => ({
+  type: "GEKLIKT",
+  data: "bla"
+});
+
+export const received_data = (data) => ({
+  type: 'RECEIVED_DATA',
+  payload: data
+})
+
+
+//thunk
+const getRepos = () => dispatch => {
+  try {
+    const url = `https://api.github.com/users/reduxjs/repos?sort=updated`;
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        console.log("thunk: getrepos data=", json);
+        dispatch(received_data(json));
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Container />
+      </div>
+    );
+  }
+}
+
+const mainRed = combineReducers({ data })
+
+const store = createStore(
+  mainRed,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 
-
-
-store.subscribe(render);
-render();
-
-
-
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

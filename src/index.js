27f82c319b30963cbd7ee6801e942b12ea626d38
Dimitri C <couwenberg@gmail.com>
2018-10-03 11:43:05 +1000

    update

--- src/index.js
+++ src/index.js
@@ -8,93 +8,49 @@ import { connect } from 'react-redux';
 import thunk from 'redux-thunk';
 
 
-class Dumb extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('Dumb props passed on from container:', props)
-  }
-
-  handleClick = () => {
-    console.log(this.props)
-    this.props.getData();
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>hier</button>
-        <ul>
-          {this.props.data && this.props.data.map(item => <li>item.name</li>)}
-        </ul>
-      </div>
-    )
-  }
-}
-
-const mapDispatchToProps = dispatch => bindActionCreators(
-  {
-    getData,
-  },
-  dispatch,
-)
-const bla = (state) => ({
-  data: state.myData
-});
-const Container = connect(bla, mapDispatchToProps)(Dumb)
+const action = type => store.dispatch({type})
 
 
-const url = `https://api.github.com/users/reduxjs/repos?sort=updated`;
-
-const getData = () => dispatch => {
-  fetch(url).then(response => response.json())
-    .then(
-      json => {
-        debugger
-        console.log('thunk returning json:', json);
-        dispatch(receivedData(json));
-      }
-    )
-}
-
-//action
-const receivedData = (data) => ({
-  type: 'RECEIVED_DATA',
-  data
-});
-
-//reducer
-const myData = (state = [], action) => {
-  debugger;
-  if (action.type === 'RECEIVED_DATA') {
-    return action.data;
-  }
-  return state;
+const Counter = ({onIncrement,value}) =>{
+  return <div>
+    {value}
+    <button onClick={onIncrement}>increase</button>
+  </div>
 }
 
 
+export const increment = (type) => ({
+  type:'INCREMENT'
+})
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Container />
-      </div>
-    );
+
+const reducer=(state = 0, action) => {
+  switch (action.type) {
+
+  case 'INCREMENT':
+  console.log('reducer: increment')
+    return state +1
+
+  default:
+    return state
   }
 }
 
-
-const store = createStore(
-  myData,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
-)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Counter
+    onIncrement={() => action(increment)}
+     />
   </Provider>,
   document.getElementById('root')
 );
 
 registerServiceWorker();
+
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+)
